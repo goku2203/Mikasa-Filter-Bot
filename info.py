24c7228a54061804a3f6584dc9171ee7f08989da
goku2203 <techgokul4@gmail.com>
2026-01-20 21:05:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -34,7 +34,7 @@ PICS = (environ.get('PICS', 'https://i.ibb.co/p6SmhYv3/photo.jpg https://i.ibb.c
 
 # Admins, Channels & Users
 ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '600302393').split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001999941677').split()]
+CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001999941677 -1002591922002').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 auth_grp = environ.get('AUTH_GROUP')
