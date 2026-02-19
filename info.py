9fb7949553471b2bca526cffce868af114c4cb28
goku2203 <techgokul4@gmail.com>
2026-02-19 09:59:59 +0530

    Rename environment variable from 'None' to 'AUTH_GROUP'

--- info.py
+++ info.py
@@ -39,7 +39,7 @@ ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ
 CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1001999941677 -1002591922002').split()]
 auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
 AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_grp = environ.get('None')
+auth_grp = environ.get('AUTH_GROUP')
 DEFAULT_AUTH_CHANNELS = [int(x) for x in environ.get("AUTH_CHANNEL", '').split() if x.lstrip('-').isdigit()]
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
